c7858a4659fc3c6a0e9f6addcbb6c1b314844739
LukeMonsf <luke@monsfort.com>
2026-06-02 15:33:58 +0200

    pixi environment changes for typedb

--- pixi_activation.sh
+++ pixi_activation.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 # Keep this file for Pixi-specific environment tweaks.
-# Do not prepend ${CONDA_PREFIX}/lib globally here: it makes system tools such
-# as /usr/bin/cmake pick up Pixi libraries like libcurl. TypeDB-based ROS nodes
-# add the path locally in their wrapper scripts instead.
+export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib:${LD_LIBRARY_PATH}"
